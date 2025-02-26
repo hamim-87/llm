288930c3edcff18f5df08f0c6fa48bb9ad3c3726
hamim-87 <hamim12109087@gmail.com>
2025-02-26 11:44:07 +0600

    initilaize weight with 0.02

--- train_model.py
+++ train_model.py
@@ -17,6 +17,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3*config.n_embd)
 
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
         self.n_head = config.n_head
         self.n_embd = config.n_embd 
@@ -51,6 +52,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4*config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -97,6 +99,25 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd)
         ))
         self.lm_head = nn.Linear(config.n_embd,config.vocab_size,bias=False)
+
+        # weight sharing scheme
+        self.transformer.wte.weight = self.lm_head.weight
+
+
+        #applying initial weights
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self,module):
+        if isinstance(module, nn.Linear):
+            std = 0.02 
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std = (2*self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean= 0.0,std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight,mean=0.0,std=0.02)  
 
 
     def forward(self, idx,targets = None):
@@ -213,32 +234,56 @@ enc = tiktoken.get_encoding('gpt2')
 
 # TRAINING PERPOSE 
 
-with open('input.txt', 'r') as f:
-    text = f.read()
+class DataLoader:
+    def __init__(self,B,T):
+        self.B = B
+        self.T = T
+
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+
+        self.current_pos = 0
+
+    def next_batch(self):
+        B,T = self.B, self.T
+
+        buf = self.tokens[self.current_pos:self.current_pos+B*T+1]
+       
+
+        x = buf[:-1].view(B,T)
+        y = buf[1:].view(B,T)
+
+        self.current_pos += B*T+1
+
+        if self.current_pos+B*T+1> len(self.tokens):
+            self.current_pos = 0
+        
+        return x,y
 
 
-tokens = enc.encode(text)
+        
 
 
-B,T = 4,32
-
-buf = torch.tensor(tokens[:B*T+1])
-buf = buf.to(device)
-x = buf[:-1].view(B,T)
-y = buf[1:].view(B,T)
+trian_loader = DataLoader(4,32)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-4)
 
 for i in range(50):
+    x,y = trian_loader.next_batch()
+    x,y = x.to(device),y.to(device)
+
     optimizer.zero_grad()
-    logits,loss = model(x,y)
+    with torch.autocast(device_type=device, dtype=torch.bfloat16):
+        logits,loss = model(x,y)
     loss.backward()
     optimizer.step()
 
 
     print(f"step {i}: loss: {loss}")
 
-import sys; sys.exit(0)
+
 
 
 
@@ -275,7 +320,7 @@ def generate(x,max_length, num_return_sequences):
     while x.size(1) < max_length:
 
         with torch.no_grad():
-            logits = model(x) #(B,T,vocab_size)
+            logits,loss = model(x) #(B,T,vocab_size)
 
             logits = logits[:,-1,:] #(B,vocab_size) 
 
